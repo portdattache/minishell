8bafb4fbda453880f40bc50c467119ff16c0ac65
Baptiste Caumont <bcaumont@c2r2p1.42lehavre.fr>
2025-06-04 13:38:49 +0200

    va te faire foutregit add .

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,12 +6,14 @@
 /*   By: bcaumont <bcaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:20:24 by garside           #+#    #+#             */
-/*   Updated: 2025/06/04 10:56:40 by bcaumont         ###   ########.fr       */
+/*   Updated: 2025/06/04 13:37:53 by bcaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+// ========================== 🔹 DÉFINITIONS GÉNÉRALES =========================
 
 # include "../lib/libft.h"
 # define SUCCESS 0
@@ -24,6 +26,8 @@
 # define PROMPT "\033[1;32mminishell$> \033[0m"
 
 extern volatile sig_atomic_t	g_status;
+
+// ======================== 🧠 STRUCTURES DE DONNÉES ==========================
 
 typedef enum e_token_type
 {
@@ -81,104 +85,70 @@ typedef struct s_data
 	int							token_count;
 }								t_data;
 
-// signaux
-void							reset_signals_child(void);
+// ============================ 🧩 LEXING =====================================
 
-// parse
-t_env							*env_new(char *name, char *value);
-void							free_env_list(t_env *new_list);
-t_token							*get_next_token(t_data *data, int *i);
 t_token							*ft_lexer(t_data *data);
-int								parse(t_data *data);
-
-// lexer
+t_token							*get_next_token(t_data *data, int *i);
+t_token							*new_token(char *value, t_TokenType type);
+t_token							*handle_cmd_or_arg(t_data *data, int *i);
+t_token							*handle_pipe(int *i);
+t_token							*handle_redirection(char *input, int *i);
+t_token							*handle_double_redir(char *input, int *i);
 char							*handle_quotes_part(t_data *data, int *i,
 									char *value);
-t_token							*handle_cmd_or_arg(t_data *data, int *i);
-t_token							*handle_double_redir(char *input, int *i);
-t_token							*handle_redirection(char *input, int *i);
-void							skip_whitespace(const char *input, int *i);
-
-// lexer1
-void							free_one_token(t_token *token);
-void							free_token(t_token *head);
-t_token							*new_token(char *value, t_TokenType type);
-char							*handle_error_code(t_data *data, char *value,
-									int *i);
-void							add_token_to_list(t_token **head,
-									t_token **last, t_token *new_token);
-
-// lexer2
-int								is_skippable_char(char c);
-int								is_token_char(char c);
 char							*handle_env_value(t_data *data, int *i,
 									char *value);
 char							*handle_plain_text(t_data *data, int *i,
 									char *value);
-t_token							*handle_pipe(int *i);
-
-// quote
-char							*append_env_variable(t_data *data,
-									char *extract, int *i, int *first);
-char							*append_remaining_segment(t_data *data,
-									char *extract, int first, int i);
-char							*extract_word_double(t_data *data, int *i);
-char							*extract_word_single(char *input, int *i);
 char							*handle_quotes(t_data *data, int *i);
+void							skip_whitespace(const char *input, int *i);
+int								is_token_char(char c);
+int								is_skippable_char(char c);
 
-// quote1
-int								check_quotes(char *input);
-char							*ft_get_env(char *str, t_data *data);
-char							*change_env(t_data *data, int *i);
-char							*append_error_code(t_data *data, char *extract,
-									int *i, int *first);
+// ============================ 🧪 PARSING ====================================
 
-// exec
-char							*get_cmd_path(t_data *data, char **cmd);
-int								exec_child_process(t_data *data, t_cmd *cmd,
-									int prev_fd);
-int								ft_shell(t_data *data, t_cmd *cmd, int prev_fd);
-int								which_command(t_data *data, t_cmd *cmd,
-									int prev_fd);
+int								parse(t_data *data);
+t_cmd							*parse_tokens(t_data *data);
+void							add_arg(t_cmd *cmd, char *value);
+t_cmd							*new_cmd_node(void);
+void							add_token_to_list(t_token **head,
+									t_token **last, t_token *new);
+
+// ============================ 🛠️ EXECUTION =================================
+
+int								exec_line(t_data *data, t_cmd *cmd);
 int								handle_single_command(t_data *data, t_cmd *cmd,
 									int prev_fd);
 void							handle_useless_command(t_cmd *cmd,
 									int *prev_fd);
 int								wait_for_children(pid_t last_pid);
 void							maybe_close(t_cmd *cmd, int *prev_fd);
-int								exec_line(t_data *data, t_cmd *cmd);
+void							exec_child(t_data *data, t_cmd *cmd,
+									int prev_fd);
+int								which_command(t_data *data, t_cmd *cmd,
+									int prev_fd);
+int								resolve_command_path(t_data *data, t_cmd *cmd);
+int								ft_process(t_data *data, t_cmd *cmd,
+									int prev_fd);
+int								exec_child_process(t_data *data, t_cmd *cmd,
+									int prev_fd);
+int								ft_shell(t_data *data, t_cmd *cmd, int prev_fd);
+void							handle_path_exec(t_data *data, t_cmd *cmd);
+void							handle_direct_exec(t_data *data, t_cmd *cmd);
+void							ft_execve_child(t_data *data, t_cmd *cmd,
+									int input_fd, int output_fd);
+void							ft_restore_std(int input_fd, int output_fd);
+void							ft_check_directory(t_data *data, t_cmd *cmd);
+void							ft_exit_with_error(t_data *data, t_cmd *cmd,
+									char *msg, int code);
+void							ft_exit_exec(int code, t_data *data,
+									t_cmd *cmd);
+int								run_builtin(t_data *data, t_cmd *cmd);
+int								ft_executables(t_data *data, t_cmd *cmd,
+									int input_fd, int output_fd);
 
-// parse
-void							print_cmds(t_cmd *cmd);
-t_cmd							*parse_tokens(t_data *data);
-void							add_arg(t_cmd *cmd, char *value);
-t_cmd							*new_cmd_node(void);
-void							free_cmd_list(t_data *data);
+// ============================ 📦 BUILTINS ===================================
 
-// exec1
-void							free_data(t_data *data);
-char							**ft_get_cmd(t_data *data);
-void							ft_replace_in_env(t_data *data, char *name,
-									char *value);
-
-// export
-int								ft_is_valid(char *str);
-int								check_name(char *str, t_env *node,
-									char *content);
-void							free_name_content(char *name, char *content);
-char							*get_content(char *str, int i);
-
-// char	*find_cmd_path(char *cmd, t_data *data);
-void							ft_lstadd_back_env(t_env **lst, t_env *new);
-char							**translate_in_tab(t_data *data);
-int								calcul_dynamique_len(t_env *tmp);
-void							free_split(char **tmp);
-t_env							*init_env_list(char **env);
-
-//--------gestion des signaux---------
-void							init_signal(void);
-
-//-------------builtins---------------
 int								ft_pwd(void);
 int								ft_cd(t_data *data);
 int								ft_env(t_data *data);
@@ -186,77 +156,86 @@ int								ft_echo(t_data *data, t_cmd *cmd);
 int								ft_exit(t_data *data, t_cmd *cmd, int stdin,
 									int stdout);
 int								ft_isalldigit(char *str);
-
-// ryew
-int								ft_executables(t_data *data, t_cmd *cmd,
-									int input_fd, int output_fd);
 int								ft_export(t_data *data);
-void							sort(char **tmp);
-t_env							*init_export_list(char **env);
 int								ft_unset(t_data *data);
-char							*find_cmd_path(char *cmd, t_data *data);
-
-// pipe
 bool							is_builtin(char *cmd);
-int								run_builtin(t_data *data, t_cmd *cmd);
-void							exec_child(t_data *data, t_cmd *cmd,
-									int prev_fd);
-int								resolve_command_path(t_data *data, t_cmd *cmd);
-int								ft_process(t_data *data, t_cmd *cmd,
-									int prev_fd);
 
-// ft_pipe1
-void							ft_exit_exec(int code, t_data *data,
-									t_cmd *cmd);
+// ========================== 🌐 ENVIRONNEMENT ================================
+
+t_env							*env_new(char *name, char *value);
+t_env							*init_env_list(char **env);
+t_env							*init_export_list(char **env);
+void							ft_replace_in_env(t_data *data, char *name,
+									char *value);
+void							ft_lstadd_back_env(t_env **lst, t_env *new);
+char							**translate_in_tab(t_data *data);
+int								calcul_dynamique_len(t_env *tmp);
+char							*ft_get_env(char *str, t_data *data);
+void							sort(char **tmp);
+
+// ====================== 📁 REDIRECTIONS & FICHIERS ==========================
+
 int								open_infile(char *str);
-void							handle_direct_exec(t_data *data, t_cmd *cmd);
-void							handle_path_exec(t_data *data, t_cmd *cmd);
-void							handle_invalid_command(t_data *data, t_cmd *cmd,
-									int prev_fd);
-
-// ft_pipe2
-void							safe_close(int fd);
-int								redirect_management(t_cmd *cmd, int prev_fd);
-
-// pipe utils
-int								open_infile(char *str);
-int								last_infile(t_cmd *cmd);
-int								manag_infile(t_cmd *cmd, int prev_fd);
 int								open_outfile(char *file, t_TokenType mode);
+int								last_infile(t_cmd *cmd);
 int								last_outfile(t_cmd *cmd);
+int								manag_infile(t_cmd *cmd, int prev_fd);
 int								manag_outfile(t_cmd *cmd, int *pipe_fd);
+int								redirect_management(t_cmd *cmd, int prev_fd);
+int								set_fd_cloexec(int fd);
 
-// error message
+// ============================ 🐚 HERE-DOC ===================================
+
+void							made_new_file(int *fd, char **name);
+void							fill_here_doc_file(int fd, char *delimitor);
+char							*get_here_doc(char *str);
+
+// ============================= ⚠️ ERREURS ==================================
+
 void							command_not_found(char *cmd);
 void							no_such_file_or_directory(char *cmd);
 void							permission_denied(char *file);
 void							error_message(char *str);
 void							is_a_directory(char *str);
-int								set_fd_cloexec(int fd);
-
-// heredoc
-void							made_new_file(int *fd, char **name);
-void							fill_here_doc_file(int fd, char *delimitor);
-char							*get_here_doc(char *str);
-void							handle_sigint(int sig);
-void							reset_signals_child(void);
-
-// Utils & remise a la norme
-char							*ft_strjoin_three(char *s1, char *s2, char *s3);
-void							ft_execve_child(t_data *data, t_cmd *cmd,
-									int input_fd, int output_fd);
-void							ft_restore_std(int input_fd, int output_fd);
-void							ft_check_directory(t_data *data, t_cmd *cmd);
-void							ft_exit_with_error(t_data *data, t_cmd *cmd,
-									char *msg, int code);
-void							init_data(t_data *data);
-void							free_redir_list(t_redir *redir);
-void							add_redir(t_redir **redir_list, char *filename,
-									int type);
-void							ft_lstadd_back_env(t_env **lst, t_env *new);
 char							*check_direct_access(char *cmd);
 void							print_cmd_error(char *cmd);
+
+// ============================ 📊 UTILITAIRES ================================
+
+char							*ft_strjoin_three(char *s1, char *s2, char *s3);
 char							*try_paths(char **paths, char *cmd);
+char							*get_cmd_path(t_data *data, char **cmd);
+char							*change_env(t_data *data, int *i);
+char							*append_env_variable(t_data *data,
+									char *extract, int *i, int *first);
+char							*append_remaining_segment(t_data *data,
+									char *extract, int first, int i);
+char							*extract_word_double(t_data *data, int *i);
+char							*extract_word_single(char *input, int *i);
+char							*handle_error_code(t_data *data, char *value,
+									int *i);
+char							*get_content(char *str, int i);
+int								ft_is_valid(char *str);
+int								check_name(char *str, t_env *node,
+									char *content);
+void							free_name_content(char *name, char *content);
+
+// ======================= 🔧 SIGNALS & SHELL MGMT ============================
+
+void							init_signal(void);
+void							handle_sigint(int sig);
+void							reset_signals_child(void);
+void							init_data(t_data *data);
 void							is_not_path(t_data *data);
+
+// =========================== 🧹 LIBÉRATION MEMOIRE ==========================
+
+void							free_split(char **tmp);
+void							free_env_list(t_env *new_list);
+void							free_redir_list(t_redir *redir);
+void							free_cmd_list(t_data *data);
+void							free_token(t_token *head);
+void							free_one_token(t_token *token);
+void							free_data(t_data *data);
 
 #endif
--- minishell/src/lexer_util.c
+++ minishell/src/lexer_util.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lexer_utils_1.c                                 :+:      :+:    :+:   */
+/*   lexer_util.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bcaumont <bcaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 18:49:50 by garside           #+#    #+#             */
-/*   Updated: 2025/06/03 17:10:58 by bcaumont         ###   ########.fr       */
+/*   Updated: 2025/06/04 12:42:37 by bcaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,50 @@ void	add_token_to_list(t_token **head, t_token **last, t_token *new_token)
 	else
 		(*last)->next = new_token;
 	*last = new_token;
+}
+
+char	*handle_env_value(t_data *data, int *i, char *value)
+{
+	char	*tmp;
+	char	*temp;
+
+	if (data->input[*i + 1] == '?')
+		return (handle_error_code(data, value, i));
+	tmp = change_env(data, i);
+	temp = ft_strjoin(value, tmp);
+	free(tmp);
+	free(value);
+	return (temp);
+}
+
+char	*handle_plain_text(t_data *data, int *i, char *value)
+{
+	int		start;
+	int		len;
+	char	*tmp;
+	char	*temp;
+
+	start = *i;
+	while (is_skippable_char(data->input[*i]))
+		(*i)++;
+	len = *i - start;
+	tmp = ft_substr(data->input, start, len);
+	temp = ft_strjoin(value, tmp);
+	free(tmp);
+	free(value);
+	return (temp);
+}
+
+t_token	*handle_pipe(int *i)
+{
+	char	*value;
+	t_token	*token;
+
+	value = ft_strdup("|");
+	if (!value)
+		return (NULL);
+	token = new_token(value, PIPE);
+	(*i)++;
+	free(value);
+	return (token);
 }
